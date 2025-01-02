eb85bc13a4aca27e2441f483fc62bc92f5e67acb
Erikli15 <145444252+Erikli15@users.noreply.github.com>
2025-01-02 14:03:48 +0100

    komma in med google log in

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,20 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  ngOnInit(): void {
+    // Get the button with ID 'loginButton'
+    const loginButton: HTMLElement | null = document.getElementById('loginButton');
+
+    // Add an event listener for the click event
+    if (loginButton) {
+      loginButton.addEventListener('click', function() {
+        // Redirect the user to the backend endpoint for Google authentication
+        window.location.href = 'http://localhost:3000/auth/google';
+      });
+    } else {
+      console.error('Knappen med ID "loginButton" hittades inte.');
+    }
+  }
 }
